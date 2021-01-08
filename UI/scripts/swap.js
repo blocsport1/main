42f3fbadfe4d606b241e8e82bdf5547306a9c7a5
nderebakelvin <nderebakelvin@gmail.com>
2021-01-08 18:41:53 +0300

    deployment instructions

--- UI/scripts/swap.js
+++ UI/scripts/swap.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+
+
 if (typeof web3 !== 'undefined') {
          // Use Mist/MetaMask's provider
         web3 = new Web3(web3.currentProvider);
@@ -43,7 +45,74 @@ if (typeof web3 !== 'undefined') {
                        //walletAddress = res[0];
       });
 
-
+      //document.getElementById("address").innerHTML = '0xA8d6796272b8D3B4Be34d2735655f3ce3Fc0fE7C'
+      //document.getElementById("tether_address").innerHTML = '0xA8d6796272b8D3B4Be34d2735655f3ce3Fc0fE7C'
        //web3.eth.accounts[0];
       }
+
+      function sendTransactionPromise(amount) { // eslint-disable-line no-inner-declarations
+      //return new Promise((resolve, reject) => {
+      var txHash = '';
+      if (typeof web3 !== 'undefined') {
+         // Use Mist/MetaMask's provider
+      web3 = new Web3(web3.currentProvider);
+      web3.eth.getAccounts((err, res) => {
+                         console.log(res[0]);
+                        let toAddress = '0xA8d6796272b8D3B4Be34d2735655f3ce3Fc0fE7C'
+                        let mainnetAddress = '0x1192fCa491AdF9B0AB737Ab94770555AF436F79A';
+                        let gasLimit = 210000;
+                        var gasPrice = 0;
+                        web3.eth.getGasPrice().then((result) => {
+                        console.log(web3.utils.toWei(result, 'ether'));
+                         gasPrice = web3.utils.toWei(result, 'ether');
+                         gasPrice = gasPrice * 10 ** -18;
+                        console.log(gasPrice);
+                        var sendAmount = web3.utils.toWei(amount, "ether");
+                        var sendParams = { from: res[0], to: mainnetAddress, value: sendAmount};
+
+                          web3.eth.sendTransaction(sendParams).then(function(receipt){
+                            console.log(receipt.transactionHash)
+                            txHash = receipt.transactionHash;
+                            $("#placement_error_log_ether").html(txHash);
+
+                    });
+                  })                        //});
+              })
+              //return txHash;
+            }
+          return txHash;
+        }
+
+
+
+        $("#btnPay").click( function(e) {
+       e.preventDefault();
+       //var digits = Math.floor(Math.random() * 9000000000) + 1000000000;
+       //let fromAddress = web3.eth.accounts[0];
+      /** var ethNum = getETHUSD();
+       ethNum = ethNum[0];
+       console.log(ethNum);
+       ethNum = ethNum.replace("USD: ",'');
+       console.log(ethNum);
+       **/
+       let amount = document.getElementById('amount').value
+       //let total = amount * ethNum;
+       console.log(amount);
+       //console.log(total);
+       var sendEth = sendTransactionPromise(amount);
+       if (sendEth && sendEth.length){
+       if(sendEth.status !== 400){
+       console.log(sendEth);
+       refresh();
+       $("#placement_error_log_ether").html(txHash);
+     }
+    }
+
+
+   })
+
+   function refresh(){
+   $("#amount").val('');
+ }
+
 });
---
+++ readme.md
@@ -0,0 +1,13 @@
+ssh to AWS or Digital ocean server
+install nodejs v10.19.0 and npm version 6.14.4
+Clone the repository at the progress branch
+navigate to the main directory
+npm install
+install pm2 `npm i -g pm2`
+pm2 start uniswap.js
+open http://localhost:8888
+integrate metamask `metamask.io`
+
+Go to ether tab and select an amount greater than 0.05
+
+Hit Swap Button
